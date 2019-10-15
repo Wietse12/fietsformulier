f6e487a8ff6568b6c1c1a1f37feff17f8ae7d9f0
Wietse12 <Wietse12@users.noreply.github.com>
2019-10-15 11:30:53 +0200

    samen met peter

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
-    
-  });
+  
+  // reageer op change event op radio button
+  $("[name='fietstype']").on("change", function(){
+    var checked = $("[name='fietstype']:checked").val();
+    console.log(checked);
+
+    if(checked == "Stadsfiets dames"){
+      alert('true');
+      $("[for='Maat 61']").hide();
+      $("#Maat61").hide();
+
+    }else{
+      alert('not true');
+    }
+
+  })
+
+
+
+});
