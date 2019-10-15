5c3dd449b9886c2336d27fe127b5779c6f2343d4
Wietse12 <Wietse12@users.noreply.github.com>
2019-10-15 12:14:48 +0200

    eind versie

--- script.js
+++ script.js
@@ -15,6 +15,12 @@ $(document).ready(function() {
 
       $("[for='Geen maat']").hide();
       $("#Geen-maat").hide();
+
+      $("[for='Maat 50']").show();
+      $("#Maat50").show();
+
+      $("[for='Maat 57']").show();
+      $("#Maat57").show();
     }
 
     if(checked == "Stadsfiets heren"){
@@ -24,6 +30,69 @@ $(document).ready(function() {
 
       $("[for='Geen maat']").hide();
       $("#Geen-maat").hide();
+
+      $("[for='Maat 50']").show();
+      $("#Maat50").show();
+
+      $("[for='Maat 57']").show();
+      $("#Maat57").show();
+
+      $("[for='Maat 61']").show();
+      $("#Maat61").show();
+    }
+
+    if(checked == "Electrische fiets dames"){
+      alert('true');
+      $("[for='Maat 61']").hide();
+      $("#Maat61").hide();
+
+      $("[for='Geen maat']").hide();
+      $("#Geen-maat").hide();
+
+      $("[for='Maat 43']").show();
+      $("#Maat43").show();
+
+      $("[for='Maat 50']").show();
+      $("#Maat50").show();
+
+      $("[for='Maat 57']").show();
+      $("#Maat57").show();
+    }
+
+    if(checked == "Electrische fiets heren"){
+      alert('true');
+      $("[for='Maat 43']").hide();
+      $("#Maat43").hide();
+
+      $("[for='Geen maat']").hide();
+      $("#Geen-maat").hide();
+
+      $("[for='Maat 50']").show();
+      $("#Maat50").show();
+
+      $("[for='Maat 57']").show();
+      $("#Maat57").show();
+
+      $("[for='Maat 61']").show();
+      $("#Maat61").show();
+    }
+
+    if(checked == "Vouwfiets"){
+      alert('true');
+      $("[for='Maat 43']").hide();
+      $("#Maat43").hide();
+
+      $("[for='Maat 50']").hide();
+      $("#Maat50").hide();
+
+      $("[for='Maat 57']").hide();
+      $("#Maat57").hide();
+
+      $("[for='Maat 61']").hide();
+      $("#Maat61").hide();
+
+      $("[for='Geen maat']").show();
+      $("#Geen-maat").show();
     }
   })
 })
